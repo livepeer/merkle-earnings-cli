6a6cb02d6b2e6d8dea53906d358a41cb6f4f9c7c
Nico Vergauwen <vergauwennico@gmail.com>
2020-09-15 00:02:57 +0200

    gitignore /lib

--- lib/eth/contracts.d.ts
+++
@@ -1,4 +0,0 @@
-import { ethers } from 'ethers';
-export declare const bondingManager: ethers.Contract;
-export declare const roundsManager: ethers.Contract;
-export declare const merkleSnapshot: ethers.Contract;
