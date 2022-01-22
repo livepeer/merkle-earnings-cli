f2bc5eb2f78298e77974a3953507b6dc8622d7d2
Yondon Fu <yondon.fu@gmail.com>
2022-01-22 18:18:02 -0500

    Use MerkleSnapshot on L2

--- src/eth/provider.ts
+++ src/eth/provider.ts
@@ -1,5 +1,9 @@
 import {providers} from 'ethers'
 
-const provider = new providers.JsonRpcProvider(process.env.ETH_RPC)
+const l1Provider = new providers.JsonRpcProvider(process.env.L1_ETH_RPC)
+const l2Provider = new providers.JsonRpcProvider(process.env.L2_ETH_RPC)
 
-module.exports = provider
+module.exports = {
+    l1Provider,
+    l2Provider
+}
